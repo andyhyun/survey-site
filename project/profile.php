215d473b6b0e853b54ab75d5c374807c5fe3515b
ak2572 <ak2572@njit.edu>
2020-10-31 15:31:37 -0400

    The correct current/existing password must be entered to change password

--- project/profile.php
+++ project/profile.php
@@ -86,16 +86,29 @@ if (isset($_POST["saved"])) {
                 flash("Enter your existing password to reset password");
             }
             else if ($_POST["password"] == $_POST["confirm"]) {
-                $password = $_POST["password"];
-                $hash = password_hash($password, PASSWORD_BCRYPT);
-                //this one we'll do separate
-                $stmt = $db->prepare("UPDATE Users set password = :password where id = :id");
-                $r = $stmt->execute([":id" => get_user_id(), ":password" => $hash]);
-                if ($r) {
-                    flash("Reset password");
-                }
-                else {
-                    flash("Error resetting password");
+                $stmt = $db->prepare("SELECT password from USERS WHERE id = :id LIMIT 1");
+                $stmt->execute([":id" => get_user_id()]);
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result && isset($result["password"])) {
+                    $password_hash_from_db = $result["password"];
+                    $existing = $_POST["existing"];
+                    if(password_verify($existing, $password_hash_from_db)) {
+                        $password = $_POST["password"];
+                        $hash = password_hash($password, PASSWORD_BCRYPT);
+                        //this one we'll do separate
+                        $stmt = $db->prepare("UPDATE Users set password = :password where id = :id");
+                        $r = $stmt->execute([":id" => get_user_id(), ":password" => $hash]);
+                        if ($r) {
+                            flash("Reset password");
+                        }
+                        else {
+                            flash("Error resetting password");
+                        }
+                    }
+                    else {
+                        flash("The existing password you entered is incorrect. Please try again.");
+                    }
+
                 }
             }
             else {
