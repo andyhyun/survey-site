2c41ffc7efac97b70fd29c512e17b1ef1a1d121a
ak2572 <ak2572@njit.edu>
2020-11-14 15:43:03 -0500

    Fixed typo with nav buttons

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -11,10 +11,10 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="<?php echo get_url("register.php"); ?>">Register</a></li>
         <?php endif; ?>
         <?php if(has_role("Admin")): ?>
-            <li><a href="<?php echo get_url("test_create_survey.php"); ?>">Create Survey</a></li>
-            <li><a href="<?php echo get_url("test_list_survey.php"); ?>">View Surveys</a></li>
-            <li><a href="<?php echo get_url("test_create_question.php"); ?>">Create Question</a></li>
-            <li><a href="<?php echo get_url("test_list_questions.php"); ?>">View Questions</a></li>
+            <li><a href="<?php echo get_url("test/test_create_survey.php"); ?>">Create Survey</a></li>
+            <li><a href="<?php echo get_url("test/test_list_survey.php"); ?>">View Surveys</a></li>
+            <li><a href="<?php echo get_url("test/test_create_question.php"); ?>">Create Question</a></li>
+            <li><a href="<?php echo get_url("test/test_list_questions.php"); ?>">View Questions</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="<?php echo get_url("profile.php"); ?>">Profile</a></li>
