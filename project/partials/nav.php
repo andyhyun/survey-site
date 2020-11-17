6fc1df75672951ae6851bc658a6713c7a08c6bd3
ak2572 <ak2572@njit.edu>
2020-11-17 15:18:40 -0500

    change of text color for nav bar

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -20,22 +20,22 @@ require_once(__DIR__ . "/../lib/helpers.php");
         crossorigin="anonymous"></script>
 
 <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark text-white">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <ul class = "navbar-nav mr-auto">
             <li class="nav-item"><a class="nav-link" href="<?php echo get_url("home.php"); ?>">Home</a></li>
             <?php if (!is_logged_in()): ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("login.php"); ?>">Login</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("register.php"); ?>">Register</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("login.php"); ?>">Login</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("register.php"); ?>">Register</a></li>
             <?php endif; ?>
             <?php if(has_role("Admin")): ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("test/test_create_survey.php"); ?>">Create Survey</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("test/test_list_survey.php"); ?>">View Surveys</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("test/test_create_question.php"); ?>">Create Question</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("test/test_list_questions.php"); ?>">View Questions</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("test/test_create_survey.php"); ?>">Create Survey</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("test/test_list_survey.php"); ?>">View Surveys</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("test/test_create_question.php"); ?>">Create Question</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("test/test_list_questions.php"); ?>">View Questions</a></li>
             <?php endif; ?>
             <?php if (is_logged_in()): ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("profile.php"); ?>">Profile</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo get_url("logout.php"); ?>">Logout</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("profile.php"); ?>">Profile</a></li>
+                <li class="nav-item text-light"><a class="nav-link" href="<?php echo get_url("logout.php"); ?>">Logout</a></li>
             <?php endif; ?>
         </ul>
     <nav>
