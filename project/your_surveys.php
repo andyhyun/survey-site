28e644b56f1bea2e3d8e633b94b479050405c07c
ak2572 <ak2572@njit.edu>
2020-11-17 17:56:07 -0500

    Got rid of extra code

--- project/your_surveys.php
+++ project/your_surveys.php
@@ -42,7 +42,6 @@ else {
                     <div>
                         <div>Visibility: <?php get_visibility($r["visibility"]); ?></div>
                     </div>
-                    <div> </div>
                     <div>
                         <a type="button" class="btn btn-primary" href="<?php echo get_url("test/test_edit_survey.php"); ?>?id=<?php safer_echo($r['id']); ?>">Edit</a>
                         <a type="button" class="btn btn-primary" href="<?php echo get_url("test/test_view_survey.php"); ?>?id=<?php safer_echo($r['id']); ?>">View</a>
