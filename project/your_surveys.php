88f370d07797e68105c30eaab33d3fb9d640507c
ak2572 <ak2572@njit.edu>
2020-11-17 17:38:40 -0500

    test to see what's wrong

--- project/your_surveys.php
+++ project/your_surveys.php
@@ -9,8 +9,8 @@ if(!is_logged_in()) {
 $results = [];
 $user_id = get_user_id();
 $db = getDB();
-$stmt = $db->prepare("SELECT id, title, description, visibility, user_id FROM Survey WHERE user_id = :usr_id LIMIT 10");
-$r = $stmt->execute([":usr_id" => $user_id]);
+$stmt = $db->prepare("SELECT id, title, description, visibility, user_id FROM Survey WHERE user_id = 1");
+$r = $stmt->execute();
 if ($r) {
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
