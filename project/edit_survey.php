c35a0a5b190764c4b994d48a87352327fc1fd72d
ak2572 <ak2572@njit.edu>
2020-12-14 19:17:23 -0500

    Changed how the form looks

--- project/edit_survey.php
+++ project/edit_survey.php
@@ -55,30 +55,33 @@ else {
 }
 ?>
 
-<form method="POST">
-    <div class="form-group">
-        <label for="">Title</label>
-        <input class="form-control" type="text" id="title" name="title" required maxlength="45" value="<?php echo $result["title"];?>"/>
-    </div>
-    <div class="form-group">
-        <label for="description">Description</label>
-        <textarea class="form-control" type="text" id="description" name="description" value="<?php echo $result["description"];?>"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="category">Category</label>
-        <input class="form-control" type="text" id="category" name="category" maxlength="15" value="<?php echo $result["category"];?>"/>
-    </div>
-    <div class="form-group">
-        <label for="visibility">Visibility</label>
-        <select class="form-control" name="visibility" id="visibility" required value="<?php echo $result["visibility"];?>">
-            <option value="0" <?php echo ($result["visibility"] == "0"?'selected="selected"':'');?>>Draft</option>
-            <option value="1" <?php echo ($result["visibility"] == "1"?'selected="selected"':'');?>>Private</option>
-            <option value="2" <?php echo ($result["visibility"] == "2"?'selected="selected"':'');?>>Public</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <input type="submit" style="margin-top: 30px;" name="saved" class="btn btn-primary" value="Update Survey"/>
-    </div>
-</form>
+<div class="container-fluid">
+    <h3 style="margin-top: 20px;margin-bottom: 20px;">Edit Survey</h3>
+    <form method="POST">
+        <div class="form-group">
+            <label for="">Title</label>
+            <input class="form-control" type="text" id="title" name="title" required maxlength="45" value="<?php echo $result["title"];?>"/>
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" type="text" id="description" name="description" value="<?php echo $result["description"];?>"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="category">Category</label>
+            <input class="form-control" type="text" id="category" name="category" maxlength="15" value="<?php echo $result["category"];?>"/>
+        </div>
+        <div class="form-group">
+            <label for="visibility">Visibility</label>
+            <select class="form-control" name="visibility" id="visibility" required value="<?php echo $result["visibility"];?>">
+                <option value="0" <?php echo ($result["visibility"] == "0"?'selected="selected"':'');?>>Draft</option>
+                <option value="1" <?php echo ($result["visibility"] == "1"?'selected="selected"':'');?>>Private</option>
+                <option value="2" <?php echo ($result["visibility"] == "2"?'selected="selected"':'');?>>Public</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <input type="submit" style="margin-top: 30px;" name="saved" class="btn btn-primary" value="Update Survey"/>
+        </div>
+    </form>
+</div>
 
 <?php require(__DIR__ . "/partials/flash.php"); ?>
